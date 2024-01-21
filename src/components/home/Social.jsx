127ce6b085d2df60c31aedfbf19237b9a82a5ad6
Mohamed Rawash <rawashm35@gmail.com>
2024-01-21 11:37:15 +0200

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import {} from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/_mohamed_rawash/"
+        class="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://github.com/mohamed-rawash"
+        class="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mohamed-rawash-61b30a217/"
+        class="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
